6ca7a32da17212ef9344543b7ad0013993a7b955
Juan Gustin <juandgustin@gmail.com>
2023-04-23 20:01:03 -0500

    4. Frontend - Back Up - Before Solution -> Process Is Not Defined

--- client/src/Components/Landing/Landing.jsx
+++ client/src/Components/Landing/Landing.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 //Standard
 import { Link } from 'react-router-dom';
-//import style from './Landing.module.css';
+import style from './Landing.module.css';
+import landing_dog from '../../images/dog.png';
 
 function Landing() {
   return (
-    <div>
-      <h1>DogWorld</h1>
-      <p>by GustinDev</p>
-      <Link to='/home'>
-        <button>Let's Goa!</button>
-      </Link>
+    <div className={style.container}>
+      <div className={style.text}>
+        <h1 className={style.title}>DogWorld</h1>
+        <p>by GustinDev</p>
+        <Link to='/home'>
+          <button className={style.buttonOne}>Let's Go!</button>
+        </Link>
+      </div>
+      <div className={style.image}>
+        <img src={landing_dog} alt='landing_dog' />
+      </div>
     </div>
   );
 }

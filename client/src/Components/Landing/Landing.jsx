9936746e13345f51e99df1c8824237bc7c156469
Juan Gustin <juandgustin@gmail.com>
2023-05-25 15:12:40 -0500

    Landing: Mobile V1

--- client/src/Components/Landing/Landing.jsx
+++ client/src/Components/Landing/Landing.jsx
@@ -6,9 +6,9 @@ import landing_dog from '../../images/dog.png';
 
 function Landing() {
   return (
-    <div className='flex bg-landingb justify-evenly justify-items-end w-screen h-screen'>
-      <div className='flex text-center text-bluetext flex-col justify-center items-center w-[600px] ml-[200px] mt-[-100px]'>
-        <h1 className={style.title}>DogWorld</h1>
+    <div className='flex justify-end sm:flex flex-col sm:flex bg-landingb justify-evenly justify-items-end w-screen h-screen'>
+      <div className='flex w-full h-1/2 sm:flex text-center text-bluetext flex-col justify-center items-center w-1/2'>
+        <h1 className='text-[60px] sm:text-[120px] font-lilita'>DogWorld</h1>
         <p className='text-[25px] font-lilita'>by GustinDev</p>
         <Link to='/home'>
           <button className='font-lilita inline-block cursor-pointer border-0 rounded-[10px] text-white bg-blue-500 text-[25px] leading-28 px-[30px] py-[5px] text-center mt-[30px] tracking-wide hover:bg-hoverbtn shadow-md'>
@@ -16,13 +16,14 @@ function Landing() {
           </button>
         </Link>
       </div>
-      <div className='flex items-end'>
+      <div className='w-full flex h-1/2 sm:flex items-end w-1/2 justify-center items-center'>
         <img
-          className='flex w-[700px] h-[740px]'
+          className='w-[350px] h-[360px] sm:flex w-[700px] h-[740px] '
           src={landing_dog}
           alt='landing_dog'
         />
       </div>
+      <div className='text black sm:text-white text-3xl md:text-blue'></div>
     </div>
   );
 }

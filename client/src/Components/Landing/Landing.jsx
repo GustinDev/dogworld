f1aa87ac8a30636f002fd86fc6306217eff344d7
Juan Gustin <juandgustin@gmail.com>
2023-05-25 16:36:43 -0500

    Landing: Mobile V2

--- client/src/Components/Landing/Landing.jsx
+++ client/src/Components/Landing/Landing.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 //Standard
 import { Link } from 'react-router-dom';
-import style from './Landing.module.css';
 import landing_dog from '../../images/dog.png';
 
 function Landing() {
   return (
-    <div className='flex justify-end sm:flex flex-col sm:flex bg-landingb justify-evenly justify-items-end w-screen h-screen'>
-      <div className='flex w-full h-1/2 sm:flex text-center text-bluetext flex-col justify-center items-center w-1/2'>
-        <h1 className='text-[60px] sm:text-[120px] font-lilita'>DogWorld</h1>
+    <div className='flex flex-col justify-end  sm:justify-center sm:items-center lg:flex-row  bg-landingb justify-evenly justify-items-end w-screen h-screen'>
+      <div className='flex w-full h-1/2 sm:flex text-center text-bluetext flex-col justify-center items-center sm:w-1/2'>
+        <h1 className='text-[60px] sm:text-[80px] font-lilita xl:text-[120px]'>
+          DogWorld
+        </h1>
         <p className='text-[25px] font-lilita'>by GustinDev</p>
         <Link to='/home'>
           <button className='font-lilita inline-block cursor-pointer border-0 rounded-[10px] text-white bg-blue-500 text-[25px] leading-28 px-[30px] py-[5px] text-center mt-[30px] tracking-wide hover:bg-hoverbtn shadow-md'>
@@ -16,14 +17,13 @@ function Landing() {
           </button>
         </Link>
       </div>
-      <div className='w-full flex h-1/2 sm:flex items-end w-1/2 justify-center items-center'>
+      <div className='w-full flex h-1/2 items-end justify-center sm:w-1/2 md:items-center'>
         <img
-          className='w-[350px] h-[360px] sm:flex w-[700px] h-[740px] '
+          className='w-[450px] h-[450px] sm:flex sm:w-90 sm:h-90 md:w-[450px] md:h-[450px] lg:w-[850px] lg:h-[850px]'
           src={landing_dog}
           alt='landing_dog'
         />
       </div>
-      <div className='text black sm:text-white text-3xl md:text-blue'></div>
     </div>
   );
 }

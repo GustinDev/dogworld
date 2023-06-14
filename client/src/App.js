935b72530e72121307a0c8a762f332fac8e5d04c
Juan Gustin <juandgustin@gmail.com>
2023-06-13 21:22:38 -0500

    Deploy Final - Fire

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import Home from './Components/Home/Home';
 import Detail from './Components/Detail/Detail';
 import Form from './Components/Form/Form';
 import axios from 'axios';
-axios.defaults.baseURL = 'http://localhost:3001/';
+axios.defaults.baseURL = 'https://dogworld-production.up.railway.app/';
 
 function App() {
   return (
